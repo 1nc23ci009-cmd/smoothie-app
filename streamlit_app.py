f7e0af3608b954371dad5b4d9b9626ad478e8e69
ARCOT SADIQ BASHA <1nc23ci009@ncetmail.com>
2026-08-15 14:42:14 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write title and intro text
+st.title("Customize Your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!"""
+)
+
+# Add customer name text input box
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+# Get current Snowflake session
+session = get_active_session()
+
+# Pull fruit options from Snowflake table
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+# Add multiselect widget with maximum selection limit of 5
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
+)
+
+# IF block: runs when ingredients_list is NOT empty
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    # Build SQL Insert Statement
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+
+    # Add Submit Order button
+    time_to_insert = st.button('Submit Order')
+
+    # Execute insert ONLY when the Submit Order button is clicked
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
