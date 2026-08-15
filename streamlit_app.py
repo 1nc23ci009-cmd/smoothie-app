5f13cdb472fb49f955a195468e971d063d459a62
ARCOT SADIQ BASHA <1nc23ci009@ncetmail.com>
2026-08-15 15:37:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write title and intro text
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -16,8 +17,11 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Pull fruit options from Snowflake table
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# Pull fruit options from Snowflake table, including SEARCH_ON
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+# Convert Snowpark Dataframe to Pandas Dataframe so we can use .loc
+pd_df = my_dataframe.to_pandas()
 
 # Add multiselect widget with maximum selection limit of 5
 ingredients_list = st.multiselect(
@@ -32,10 +36,14 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-        # Call API using the fruit_chosen variable dynamically
+
+        # Find the SEARCH_ON value corresponding to fruit_chosen
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+        # Call API using the search_on variable dynamically
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Build SQL Insert Statement
