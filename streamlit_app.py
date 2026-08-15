95cef31a9f7c6a1a2ce7de3cba17641ee9d16401
ARCOT SADIQ BASHA <1nc23ci009@ncetmail.com>
2026-08-15 15:30:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write title and intro text
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -43,3 +44,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+# New section to display SmoothieFroot API response
+st.subheader('Nutrition Information')
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
