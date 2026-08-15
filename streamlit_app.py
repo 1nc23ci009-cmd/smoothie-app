db46563d386495b9aa5f67bfb085b93e00822263
ARCOT SADIQ BASHA <1nc23ci009@ncetmail.com>
2026-08-15 15:34:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        # Display nutrition information for each selected fruit
+        # Call API using the fruit_chosen variable dynamically
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
